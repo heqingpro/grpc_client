7ecb1d56b84c265ac04d12846ea9ead320817df3
heqingpro <1911620005@qq.com>
2022-04-25 22:04:10 +0800

    feat: uploadfile

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module helloword
 go 1.16
 
 require (
-	github.com/agiledragon/gomonkey/v2 v2.7.0 // indirect
-	github.com/smartystreets/goconvey v1.7.2 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc v1.45.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	github.com/agiledragon/gomonkey/v2 v2.7.0
+	github.com/go-kratos/kratos/v2 v2.2.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
+	github.com/smartystreets/goconvey v1.7.2
+	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e
+	google.golang.org/grpc v1.45.0
+	google.golang.org/protobuf v1.27.1
 )
--- greeter_client.go
+++ greeter_client.go
@@ -22,8 +22,11 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"time"
+	"os"
+	// "time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -49,11 +52,67 @@ func main() {
 	c := NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &HelloRequest{Name: *name})
+	// ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	// defer cancel()
+	// r, err := c.SayHello(ctx, &HelloRequest{Name: *name})
+	// if err != nil {
+	// 	log.Fatalf("could not greet: %v", err)
+	// }
+	// log.Printf("Greeting: %s", r.GetMessage())
+
+	UploadFile(c)
+	fmt.Println("finish")
+}
+
+func UploadFile(cc GreeterClient) {
+	fmt.Println("start UploadFile")
+	file, err := os.Open("./go.mod")
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		panic(err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("UploadFile: len%d", len(content))
+	client, err := cc.UploadFile(context.TODO())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// if err := client.Send(&UploadRequest{
+	// 	Content: content,
+	// 	}); err != nil{
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// status, err := client.CloseAndRecv()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	for n := 0; n < len(content); n += 100 {
+		var buf []byte
+		if 100+n > len(content) {
+			buf = content[n:]
+		} else {
+			buf = content[n : n+100]
+		}
+		err := client.Send(&UploadRequest{
+			Content: buf,
+		})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	status, err := client.CloseAndRecv()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("uploadResp%v: ", status)
 }
